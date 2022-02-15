56cc5e9febd592807fc0b051c97a1c6e32ebe2fe
izakariyya <zakariyyaidris68@gmail.com>
2022-02-15 15:14:31 +0000

    Add files via upload

---
+++ pt737_adab.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 16 14:16:35 2021
+
+@author: 1804499
+"""
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.metrics import auc, accuracy_score, roc_auc_score, roc_curve
+import memory_profiler
+import time
+
+#Function to load, normalize and splits datasets in csv format
+
+def data():
+    benign = np.loadtxt("benign_traffic.csv", delimiter = ",")
+    mirai = np.loadtxt("mirai_traffic.csv", delimiter = ",")
+    gafgyt = np.loadtxt("gafgyt_traffic.csv", delimiter = ",")
+    alldata = np.concatenate((benign, gafgyt, mirai))
+    j = len(benign[0])
+    data = alldata[:, 1:j] 
+    benlabel = alldata[:, 0]
+    bendata = (data - data.min()) / (data.max() - data.min())
+    bendata, benmir, benlabel, benslabel = train_test_split(bendata, benlabel, test_size = 0.2, random_state = 42)
+    return bendata, benmir, benlabel, benslabel
+
+#load data
+
+train, test, trainlabel, testl = data()
+
+
+# train the classifier
+
+def train_adab(train, trainlabel):
+    iotbinclf = iotbinclf = AdaBoostClassifier()
+    trainclf = iotbinclf.fit(train, trainlabel)
+    return trainclf
+
+
+def predicted_adab(clf, test):
+    predictions = clf.predict(test)
+    acc = accuracy_score(testl, predictions)
+    return acc
+
+
+starttb = time.time()
+startmb = memory_profiler.memory_usage()
+
+clf = train_adab(train, trainlabel)
+
+endtb = time.time()
+endmb = memory_profiler.memory_usage()
+train_time_b = endtb - starttb
+train_memory_b = endmb[0] - startmb[0]
+
+print("Training time base: {:2f} sec".format(train_time_b))
+print("Training memory base: {:2f} mb".format(train_memory_b))
+
+
+starttesttb = time.time()
+starttestmb = memory_profiler.memory_usage()
+
+acc_test_b = predicted_adab(clf, test)
+
+endttestb = time.time()
+endtestmb = memory_profiler.memory_usage()
+test_time_b = endttestb - starttesttb
+test_memory_b = endtestmb[0] - starttestmb[0]
+
+print("Test set accuracy base: {:2f}".format(acc_test_b))
+print("Testing time base: {:2f} sec".format(test_time_b))
+print("Testing memory base: {:2f} mb".format(test_memory_b))
---
+++ pt737_adab_lf.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 16 14:16:35 2021
+
+@author: 1804499
+"""
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.metrics import auc, accuracy_score, roc_auc_score, roc_curve
+import random
+
+
+#Function to load, normalize and splits datasets in csv format
+
+def data():
+    benign = np.loadtxt("benign_traffic.csv", delimiter = ",")
+    mirai = np.loadtxt("mirai_traffic.csv", delimiter = ",")
+    gafgyt = np.loadtxt("gafgyt_traffic.csv", delimiter = ",")
+    alldata = np.concatenate((benign, gafgyt, mirai))
+    j = len(benign[0])
+    data = alldata[:, 1:j] 
+    benlabel = alldata[:, 0]
+    bendata = (data - data.min()) / (data.max() - data.min())
+    bendata, benmir, benlabel, benslabel = train_test_split(bendata, benlabel, test_size = 0.2, random_state = 42)
+    return bendata, benmir, benlabel, benslabel
+
+#load data
+
+train, test, trainlabel, testl = data()
+
+trainlbl = trainlabel.tolist()
+
+n = len(trainlbl)
+percent = 0.5
+
+partly_flipped = [1.0 - trainlbl[i] if i in random.sample(list(range(n)),int(percent*n)) else trainlbl[i] for i in range(n) ]
+
+partly_flipped = np.asarray(partly_flipped)
+
+
+# train the classifier
+
+def train_abdt(train, trainlabel):
+    iotbinclf = AdaBoostClassifier()
+    trainclf = iotbinclf.fit(train, trainlabel)
+    return trainclf
+
+
+def predicted_abdt(clf, test):
+    predictions = clf.predict(test)
+    acc = accuracy_score(testl, predictions)
+    return acc
+
+
+clf = train_abdt(train, partly_flipped)
+
+acc_test_b = predicted_abdt(clf, test)
+
+print("Test set accuracy base: {:2f}".format(acc_test_b))
